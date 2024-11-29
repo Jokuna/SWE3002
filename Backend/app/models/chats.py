328e00d1e4f1d86180b450b76555275a87d99258
KunHee Cho <jokuna.dev@gmail.com>
2024-11-30 04:12:02 +0900

    [Backend] Chat API

--- Backend/app/models/chats.py
+++ Backend/app/models/chats.py
@@ -7,8 +7,8 @@ from app.models.chatrooms import ChatRoom
 
 class Chat(Document):
     # id: int
-    userId1: Optional[Link[User]]  # 사용자 1 ID # 이거 _di
-    userId2: Optional[Link[User]]  # 사용자 2 ID
+    userId1: str # Optional[Link[User]]  # 사용자 1 ID
+    userId2: str # Optional[Link[User]]  # 사용자 2 ID
     chatRoomId: str # 채팅방 ID
     created_at: datetime = datetime.utcnow()
 
